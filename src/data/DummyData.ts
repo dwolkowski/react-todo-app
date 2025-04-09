f41d3c7528ec4c90dd57b030964f06910351738e
dwolkowski <darek.wolkowski@gmail.com>
2025-04-09 23:22:49 +0200

    Create type and dummy data for todo list

---
+++ src/data/DummyData.ts
@@ -0,0 +1,23 @@
+
+import { Todo } from "../types/Todo";
+
+export const DUMMYDATA: Todo[] = [
+  {
+    id: 1,
+    title: "Make francesinha",
+    description: "(tost kiełbasa kotlet boczek parówki jakieś smażone mięso ser tost jajko sadzone - frytki i wszystko ratuje tłusty sos) :) Portugalskie narodowe danie",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Study for final exam",
+    description: "University studies.",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Watch video",
+    description: "Very cool video.",
+    completed: false,
+  },
+];
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
       <main> 
-        <h1></h1>
+        <h1>TEST</h1>
       </main> 
   )
 }
